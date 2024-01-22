251cb95ca0e2b96020bd370aea89c58beebf4dd6
Skale17 <155162000+Skale17@users.noreply.github.com>
2024-01-22 14:43:22 +0100

    refactoring code base

--- base.py
+++ base.py
@@ -1,8 +1,14 @@
 import sqlite3
-
-def create_table(name):
-    conn = sqlite3.connect(f'{name}.sql')
+def connect():
+    conn = sqlite3.connect('main_base.sql')
     cur = conn.cursor()
+    return conn, cur
+def closee(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+def create_table():
+    conn, cur = connect()
     cur.execute('''
                 CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,104 +17,74 @@ def create_table(name):
                 mmr int
             )
         ''')
-    conn.commit()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
 def insert_user_info(name, id, day, mmr):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     try:
         cur.execute('INSERT INTO users(name, id,day, mmr) VALUES (?, ?, ?,?)', (name, id, day, mmr))
-        conn.commit()
-        cur.close()
-        conn.close()
+        closee(conn, cur)
         result = True
         return result
     except:
-        conn.commit()
-        cur.close()
-        conn.close()
+        closee(conn, cur)
         result = False
         return result
 
 def update_day(day, user_id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('UPDATE users SET day = ? WHERE id = ?', (day, user_id))
-    conn.commit()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
 
 def select_days_from_base(user_id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT day FROM users WHERE id = ?', (user_id,))
     result = str(cur.fetchone())
-    conn.commit()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return result
 def update_user_name(name, id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('UPDATE users SET name = ? WHERE id = ?', (name, id))
-    conn.commit()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
 def select_users():
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT * FROM users')
     users = cur.fetchall()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return users
 def select_mmr(user_id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT mmr FROM users WHERE id = ?', user_id)
     mmr = cur.fetchone()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return mmr
 def update_mmr(mmr, user_id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('UPDATE users SET mmr = ? WHERE id = ?', (mmr, user_id[0]))
     conn.commit()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
 def all_id():
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT id FROM users')
     all_users = cur.fetchall()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return all_users
 def select_user_name(user_id):
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT name FROM users WHERE id = ?', user_id)
     name = cur.fetchone()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return name
 def select_all_mmr():
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT mmr FROM users')
     mmr = cur.fetchone()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return mmr
 def select__name_where_mmr(mmr):
     mmr_i = int(mmr)
-    conn = sqlite3.connect('main_base.sql')
-    cur = conn.cursor()
+    conn, cur = connect()
     cur.execute('SELECT name FROM users WHERE mmr = ?', (mmr_i,))
 
     name = cur.fetchone()
-    cur.close()
-    conn.close()
+    closee(conn, cur)
     return name
