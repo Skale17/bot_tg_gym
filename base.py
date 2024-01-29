88d4c883c27445fbbee274c8d2626448de34a723
Skale17 <155162000+Skale17@users.noreply.github.com>
2024-01-29 19:38:11 +0100

    for last update

--- base.py
+++ base.py
@@ -7,7 +7,7 @@ def close(conn, cur):
     conn.commit()
     cur.close()
     conn.close()
-def create_table():
+def create_table_user():
     conn, cur = connect()
     cur.execute('''
                 CREATE TABLE IF NOT EXISTS users (
@@ -19,6 +19,82 @@ def create_table():
             )
         ''')
     close(conn, cur)
+def create_table_challenge():
+    conn, cur = connect()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS challenge (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                action varchar(100),
+                approaches int,
+                weight int,
+                repetitions int
+            )
+        ''')
+    close(conn, cur)
+
+def insert_action(action, approaches, weight, repetitions):
+    conn, cur = connect()
+    cur.execute('INSERT INTO challenge(action, approaches, weight, repetitions) VALUES (?, ?, ?, ?)', (action, approaches, weight, repetitions))
+    close(conn, cur)
+
+def filter_int(text):
+    return ''.join(symbol for symbol in text if symbol.isspace() or symbol.isalpha() or symbol.isdigit())
+
+def select_last_id_challenge():
+    conn, cur = connect()
+    cur.execute('SELECT id FROM challenge')
+    id = cur.fetchall()
+    id_filter = filter_int(str(id))
+    id_spl = id_filter.split()
+    id_challenge = sorted(id_spl, reverse=True)
+    close(conn, cur)
+    return id_challenge[0]
+def insert_approaches(id, approaches):
+    conn, cur = connect()
+    cur.execute('UPDATE challenge SET approaches = ? WHERE id = ?', (approaches, id))
+    close(conn, cur)
+
+def insert_weight(id, weight):
+    conn, cur = connect()
+    cur.execute('UPDATE challenge SET weight = ? WHERE id = ?', (weight, id))
+    close(conn, cur)
+
+def insert_repetitions(id, repetitions):
+    conn, cur = connect()
+    cur.execute('UPDATE challenge SET repetitions = ? WHERE id = ?', (repetitions, id))
+    close(conn, cur)
+def select_action(id):
+    conn, cur = connect()
+    cur.execute("SELECT action FROM challenge WHERE id=?", (id,))
+    data = cur.fetchone()
+    close(conn, cur)
+    return data
+def select_approaches(id):
+    conn, cur = connect()
+    cur.execute("SELECT approaches FROM challenge WHERE id=?", (id,))
+    data = cur.fetchone()
+    close(conn, cur)
+    return data
+def select_weight(id):
+    conn, cur = connect()
+    cur.execute("SELECT weight FROM challenge WHERE id=?", (id,))
+    data = cur.fetchone()
+    close(conn, cur)
+    return data
+def select_repetitions(id):
+    conn, cur = connect()
+    cur.execute("SELECT repetitions FROM challenge WHERE id=?", (id,))
+    data = cur.fetchone()
+    close(conn, cur)
+    return data
+def select_chall():
+    conn, cur = connect()
+    cur.execute('SELECT * FROM challenge')
+    challenge = cur.fetchall()
+    close(conn, cur)
+    return challenge
+
+
 def insert_user_info(name, id, day, mmr, role):
     conn, cur = connect()
     try:
